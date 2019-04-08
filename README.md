2ad8603f31827eee8296d070c2794c0f2086f6f5
Diego Cerdan <diegocerdan@gmail.com>
2019-04-08 22:41:40 +0200

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # facebook-reactions
+
+
+## Configuration
+Write your own config.py
+
+## Run
+python main.py >> post_reactions.json
--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import config
 import facebook
-
+import json
 
 graph = facebook.GraphAPI(access_token=config.page_token, version="3.0")
 
-
 latest_posts = graph.get_object(id=config.page_id, fields='posts.fields(type, name, created_time, object_id)')
+latest_posts = latest_posts['posts']['data']
 
-post_1 = latest_posts['posts']['data'][1]
+def get_all_reactions(id):
+    args = {}
+    while True:
+        objects = graph.request("{0}/{1}/reactions".format(graph.version, id), args)
+
+        for object in objects['data']:
+            yield object
+
+        next = objects.get('paging', {}).get('next')
+
+        if not next:
+            return
+
+        args['after'] = objects['paging']['cursors']['after']
 
 
-post_1_data = graph.get_object(post_1['id'], fields="reactions")
+post_reactions = {}
 
-print(post_1_data)
+for post in latest_posts:
+    reactions = get_all_reactions(post['id'])
+
+    post_reactions[post['id']] = list(reactions)
+
+print(json.dumps(post_reactions))
